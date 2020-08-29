0c2b61414a7e1524705e735d979694974d015046
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-29 01:14:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,12 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads => {:pieces => 4, :name=>{}}}
+	monopoly = {
+	  :railroads => {
+	    :pieces => 4, 
+	    :name=>{}
+	  }
+	}
 
 end
 
